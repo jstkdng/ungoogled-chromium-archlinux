b7846c1209311a1c53307381a0d2aeb7d371486a
jk <jk@vin.ovh>
2020-06-08 22:31:08 -0500

    test new workflow

---
+++ .github/workflows/wireguard_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo add-apt-repository -y ppa:wireguard/wireguard
+sudo apt -y install wireguard
+
+TMP=$(mktemp -d)
+cd $TMP
+CONF="$(printf $WGCONF | base64 -d)"
+echo "$CONF" > wg0.conf
+sudo wg-quick up $PWD/wg0.conf

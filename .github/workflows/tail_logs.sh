bef220ea0841c83e4ffd73bd145e56638040520c
jk <jk@vin.ovh>
2020-06-10 16:02:02 -0500

    test new changes

--- .github/workflows/tail_logs.sh
+++ .github/workflows/tail_logs.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-tail -F .github/workflows/logfile&
-tail -F ungoogled-chromium-83.0.4103.97-2-x86_64-prepare.log&
-tail -F ungoogled-chromium-83.0.4103.97-2-x86_64-build.log&
-tail -F ungoogled-chromium-83.0.4103.97-2-x86_64-package.log&
+
+#tail -F .github/workflows/logfile&
+tail -F logfile&
 
 while true; do
     if compgen -G "*.pkg.tar.zst" > /dev/null; then

e922118cacb0f7cebe68f9354c949f33a3722582
jk <jk@vin.ovh>
2020-06-10 14:43:36 -0500

    test communication between service

--- .github/workflows/wireguard_setup.sh
+++ .github/workflows/wireguard_setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#sudo add-apt-repository -y ppa:wireguard/wireguard
-sudo apt -y install wireguard
+sudo apt -y install wireguard-tools
 
 TMP=$(mktemp -d)
 cd $TMP
 CONF="$(printf $WGCONF | base64 -d)"
 echo "$CONF" > wg0.conf
 sudo wg-quick up $PWD/wg0.conf
+cd $HOME
+rm -r $TMP

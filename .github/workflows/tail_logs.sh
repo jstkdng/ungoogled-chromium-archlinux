e4d453e25b5a905d5d30f99887adf546dd37c2fa
jk <jk@vin.ovh>
2020-06-10 15:34:55 -0500

    update tail script

--- .github/workflows/tail_logs.sh
+++ .github/workflows/tail_logs.sh
@@ -2,4 +2,12 @@
 tail -F .github/workflows/logfile&
 tail -F ungoogled-chromium-83.0.4103.97-2-x86_64-prepare.log&
 tail -F ungoogled-chromium-83.0.4103.97-2-x86_64-build.log&
-tail -F ungoogled-chromium-83.0.4103.97-2-x86_64-package.log
+tail -F ungoogled-chromium-83.0.4103.97-2-x86_64-package.log&
+
+while true; do
+    if compgen -G "*.pkg.tar.zst" > /dev/null; then
+        exit 0
+    else
+        sleep 2
+    fi
+done

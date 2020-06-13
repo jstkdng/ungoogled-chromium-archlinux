f5a71c157bf49815c7fba4f6dee52930471fc9ea
jk <jk@vin.ovh>
2020-06-12 21:18:33 -0500

    use tmpfs for prepare_sources

--- .github/workflows/build/prepare.sh
+++ .github/workflows/build/prepare.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+shopt -s dotglob
+
+TMP=$(mktemp -d)
+
 cd $HOME
+cp -r * $TMP
+cd $TMP
 makepkg --nobuild --nodeps
 tar caf src.tar.zst src/ --remove-file -H posix --atime-preserve
+mv src.tar.zst /home/build

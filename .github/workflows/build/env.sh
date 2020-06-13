51f1eb1fa89cc4eadebde005b4539f0d848091b4
jk <jk@vin.ovh>
2020-06-12 20:55:27 -0500

    fix path

--- .github/workflows/build/env.sh
+++ .github/workflows/build/env.sh
@@ -3,4 +3,4 @@ shopt -s dotglob
 cp -r * /home/build
 chown -R build /home/build
 rm /etc/makepkg.conf
-cp .github/workflows/makepkg.conf /etc/makepkg.conf
+cp .github/workflows/build/makepkg.conf /etc/makepkg.conf

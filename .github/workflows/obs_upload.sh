8f6fa324e9edc2ec254e4d3bb5cdc5588efd6a4b
jk <jk@vin.ovh>
2020-06-24 09:18:17 -0500

    updated workflow

--- .github/workflows/obs_upload.sh
+++ .github/workflows/obs_upload.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
+set -e
 
 declare -a FILES=(PKGBUILD _service)
+declare -A PROJECTS=(
+    [master]=ungoogled-chromium
+    [ozone]=ungoogled-chromium-ozone
+    [dev]=ungoogled-chromium-git
+)
 
 # make temporary directory
 TMP=$(mktemp -d)
 cp "${FILES[@]}" $TMP
 cd $TMP
 
+# decide which project to use
+BRANCH=${GITHUB_REF##*/}
+if [ -z ${GITHUB_HEAD_REF+x} ]; then
+    BRANCH=${GITHUB_HEAD_REF##*/}
+fi
+OBS_PROJECT=${PROJECTS[$BRANCH]}
+
 source PKGBUILD
+
+# Add required dependencies for OBS
 newdeps=$(printf "'%s' " "${depends[@]}")
 makedeps=$(printf "'%s' " "${makedepends[@]}")
 
-# append required dependencies
 makedeps=$(printf '%s\n' "${makedeps//\'java-runtime-headless\'/}")
 makedeps="${makedeps}'jack' 'jre-openjdk-headless'"
 
@@ -20,6 +34,38 @@ sed -r -i \
     -e '/^depends[+]=/d' \
     -e '/^makedepends=/,/[)]$/cmakedepends=('"${makedeps}"')' \
     "PKGBUILD"
+
+# Generate _service file
+cat > "_service" << EOF
+<services>
+  <service name="download_url">
+    <param name="protocol">https</param>
+    <param name="host">commondatastorage.googleapis.com</param>
+    <param name="path">/chromium-browser-official/chromium-${pkgver}.tar.xz</param>
+    <param name="filename">chromium-${pkgver}.tar.xz</param>
+  </service>
+  <service name="download_url">
+    <param name="protocol">https</param>
+    <param name="host">github.com</param>
+    <param name="path">/foutrelis/chromium-launcher/archive/v6.tar.gz</param>
+    <param name="filename">chromium-launcher-6.tar.gz</param>
+  </service>
+  <service name="download_url">
+    <param name="protocol">https</param>
+    <param name="host">github.com</param>
+    <param name="path">/Eloston/ungoogled-chromium/archive/${_ungoogled_ver}.zip</param>
+    <param name="filename">ungoogled-chromium-${_ungoogled_ver}.zip</param>
+  </service>
+  <service name="tar_scm">
+    <param name="scm">git</param>
+    <param name="url">git://github.com/jstkdng/ungoogled-chromium-archlinux</param>
+    <param name="revision">master</param>
+    <param name="extract">chromium.install</param>
+    <param name="extract">chromium-drirc-disable-10bpc-color-configs.conf</param>
+    <param name="extract">*.patch</param>
+  </service>
+</services>
+EOF
 
 # Send files to obs
 AUTH="authorization: Basic ${OBS_AUTH}"
@@ -30,3 +76,4 @@ do
     curl -XPUT -H 'Content-Type: application/octet-stream' -H "${AUTH}" --data-binary "@${FILE}" $URL
 done
 curl -XPOST -H "${AUTH}" "${BASE_URL}" -F "cmd=commit"
+curl -XGET -H "${AUTH}" "${BASE_URL}/_log"

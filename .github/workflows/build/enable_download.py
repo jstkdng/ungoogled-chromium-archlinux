2449f505796e9f7816842e30a1e3c31dda515c65
jk <jk@vin.ovh>
2020-06-24 19:39:39 -0500

    update selenium arguments

--- .github/workflows/build/enable_download.py
+++ .github/workflows/build/enable_download.py
@@ -5,5 +5,7 @@ from selenium.webdriver.chrome.options import Options
 
 options = Options()
 options.headless = True
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options=options)
 driver.get(sys.argv[-1])
